64dc49b2d85c8a5237ca6da20bae7a89d5d719f6
YuYue <dingbolun3@gmail.com>
2025-07-27 19:39:50 +0800

    Site updated: 2025-07-27 19:39:36

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1753616081958";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1753616368869";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
